605e6b68176da9e629d3566efa0d7c2e7acb562d
Shilpi Bundela <shilpibundela@Shilpis-MacBook-Pro.local>
2016-12-12 20:31:09 -0500

    update file structure

--- Views/JS/navbar.js
+++
@@ -1,8 +0,0 @@
-$(document).ready( function(){
-  var navbarContent = document.getElementById('navbar');
-  navbarContent.innerHTML += '<div class="navbar-logo"><img src="../Images/NortheasternLogo.png" alt="logo.png"></div>'+
-                              '<ul class="docked-nav">'+
-                              '<div class="project-navbar left">'+
-                              '<a href="../Views/index.html"><li>Home</li></a></div><div class="project-navbar right">'+
-                              '<a href="../Views/tour.html"><li>Virtual Tour</li></a><a href="../Views/Convinience.html"><li>Convenience Near NEU</li></a><a href="../Views/insideNEU.html"><li>University Location</li></a><a href="../Views/inquiryform.html"><li>Inquiries</li></a></div></ul>';
-});
--- src/JS/footer.js
+++
@@ -1,7 +0,0 @@
-$(document).ready( function(){
-  var footerContent = document.getElementById('footer');
-  footerContent.innerHTML += '  <div class="media-links">'+
-      '<ul>'+
-        '<a href="tel:603-966-9039"><li>ContactUs</li></a><a href ="sms:603-966-9039"><li>SMS</li></a><a href="../About%20Developer/Views/index.html"><li>About The Developer</li></a><a href="https://www.facebook.com/northeastern"><li><img src="../Images/fb.jpg"></li></a> <a href="https://twitter.com/northeastern"><li><img src="../Images/twitter.png"></li></a> <a href="https://www.linkedin.com/company/northeastern-university"><li><img src="../Images/linkedIn.jpg"></li></a> <a href="http://myneu.neu.edu/cp/home/displaylogin"><li><img src="../Images/NEUpng.png"></li></a>'+
-      '</ul></div>'+'<div class="copyright">&copy;Shilpi Bundela 2016-17 Northeastern University</div>';
-});
